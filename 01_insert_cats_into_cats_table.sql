2d06b0d852f2f69bb7c4a22fa398c34e7d299f49
Mendel <mkonikov123@gmail.com>
2021-10-01 00:25:14 -0400

    commit tree 009c76426892183a8e32d20535e7a5f7216885af

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO cats (name, age, breed) VALUES ('Li Bub', 5, 'American Shorthair');
+INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');
